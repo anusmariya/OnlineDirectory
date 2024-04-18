b6fd9d445782ed4f566e2a8d23bf34fe3ea7c982
anusmariya <tmariya93@gmail.com>
2024-04-18 11:14:16 +0530

    Login

---
+++ src/org/mariya1/Employee.java
@@ -0,0 +1,39 @@
+package org.mariya1;
+
+public class Employee {
+	
+	public void empid() {
+		System.out.println("12");
+		
+	}
+	public void empName() {
+		System.out.println("Pushpam");
+		
+	}
+	public void empPhone() {
+		System.out.println("7806834509");
+		
+	}
+	public void empAddress() {
+		System.out.println("Theni");
+		
+	}
+	public static void main(String [] arg) {
+		Employee e=new Employee();
+		e.empid();
+		e.empName();
+		e.empPhone();
+		e.empAddress();
+				
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
